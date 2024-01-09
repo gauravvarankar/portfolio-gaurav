04a8c94de1f88c8397ba2ef3d2ac9e6ceecd75a7
gauravvarankar <140015819+gauravvarankar@users.noreply.github.com>
2024-01-09 20:24:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Add your JavaScript functionality here
+    
+    // Example: Adding a click event to the "Download Resume" button
+    const downloadButton = document.getElementById("downloadResume");
+    downloadButton.addEventListener("click", function () {
+      // Perform an action when the button is clicked
+      alert("Downloading resume...");
+    });
+    
+    // You can add more JavaScript interactions as needed.
+  });
+  
